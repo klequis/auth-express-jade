0228ee0d64339a2409cad1cca21d3b344823eb5f
klequis <klequis@gmail.com>
2018-03-09 17:45:27 -0800

    update

--- README.md
+++ README.md
@@ -5,16 +5,28 @@ Registration & Authentication using bcrypt and Passport
 
 ## Tech Stack
 - Express
-- MySQL
 - Passport
-- Jade
-
-## Tasks
-- babel
 - bcrypt
-- mysql-promise
-- ...
+- Jade
+- MySQL
 
+
+## Paths
+
+Use the below paths to test this program
+- /users/register - to register a new user
+- /users/signin - to signin
+- /protected - page will display if signed-in. Otherwise will redirect to login page
+- /logout - redirects to home '/'
+
+The below paths are used by the app
+- /success - indicates the login has succeeded
+- /fail - indicates the login has failed
+
+## Database
+
+The app uses a MySQL db named db_users with one table named tbl_users.
+Use db_users.sql to create the table.
 
 ## Partially based on
 [NodeJS Passport login script with MySQL database](https://programmerblog.net/nodejs-passport-login-mysql/)
---
+++ db_users.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 09, 2018 at 05:39 PM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.25-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `full_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `username`, `password`, `full_name`) VALUES
+(1, 'tom', '$2a$10$9Qy81/TMmk3Se8CBL4x7c.xSy/5cLGsaws9KZkvFAy5uGlJWMVu6u', 'tom');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
